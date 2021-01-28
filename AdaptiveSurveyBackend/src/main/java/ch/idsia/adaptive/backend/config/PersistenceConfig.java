953ce43762e79aeddd23ae1d85ca29fa307e6403
Claudio Bonesana <claudio.bonesana@supsi.ch>
2021-01-28 13:42:00 +0100

    Updated PersistenceConfig to support different kind of databases. Updated docker.

--- AdaptiveSurveyBackend/src/main/java/ch/idsia/adaptive/backend/config/PersistenceConfig.java
+++ AdaptiveSurveyBackend/src/main/java/ch/idsia/adaptive/backend/config/PersistenceConfig.java
@@ -1,5 +1,7 @@
 package ch.idsia.adaptive.backend.config;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.context.annotation.Bean;
@@ -29,6 +31,7 @@ import java.util.Properties;
 @EnableTransactionManagement
 @EnableJpaRepositories(basePackages = "ch.idsia.adaptive.backend.persistence.dao")
 public class PersistenceConfig {
+	public static final Logger logger = LogManager.getLogger(PersistenceConfig.class);
 
 	private final Environment env;
 
@@ -37,13 +40,63 @@ public class PersistenceConfig {
 		this.env = env;
 	}
 
+	private void configurePostgreSQL(DriverManagerDataSource ds) {
+		logger.info("Database platform: Postgres");
+
+		final String hostname = env.getProperty("spring.datasource.hostname", "localhost");
+		final String port = env.getProperty("spring.datasource.port", "5432");
+		final String schema = env.getProperty("spring.datasource.schema", "adaptive");
+		final String connUrl = "jdbc:postgresql://" + hostname + ":" + port + "/" + schema;
+
+		ds.setDriverClassName("org.postgresql.Driver");
+		ds.setUrl(connUrl);
+	}
+
+	private void configureMySQL(DriverManagerDataSource ds) {
+		logger.info("Database platform: MySQL");
+
+		final String hostname = env.getProperty("spring.datasource.hostname", "localhost");
+		final String port = env.getProperty("spring.datasource.port", "3306");
+		final String schema = env.getProperty("spring.datasource.schema", "adaptive");
+		final String connUrl = "jdbc:mysql://" + hostname + ":" + port + "/" + schema;
+
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl(connUrl);
+	}
+
+	private void configureMemory(DriverManagerDataSource ds) {
+		logger.info("Database platform: Memory");
+
+		final String schema = env.getProperty("spring.datasource.schema", "");
+		final String connUrl = "jdbc:h2:mem:" + schema + ";DB_CLOSE_DELAY=-1;DATABASE_TO_UPPER=false;";
+
+		ds.setDriverClassName("org.h2.Driver");
+		ds.setUrl(connUrl);
+	}
+
 	@Bean
 	public DataSource dataSource() {
 		DriverManagerDataSource ds = new DriverManagerDataSource();
-		ds.setDriverClassName(env.getProperty("spring.datasource.driverClassName", ""));
-		ds.setUrl(env.getProperty("spring.datasource.url", ""));
 		ds.setUsername(env.getProperty("spring.datasource.username", ""));
 		ds.setPassword(env.getProperty("spring.datasource.password", ""));
+
+		final String dbms = env.getProperty("spring.datasource.dbms", "");
+
+		switch (dbms) {
+			case "postgresql":
+				configurePostgreSQL(ds);
+				break;
+
+			case "mysql":
+				configureMySQL(ds);
+				break;
+
+			default:
+				logger.warn("Invalid DBMS={}, using in memory", dbms);
+			case "memory":
+				configureMemory(ds);
+				break;
+		}
 		return ds;
 	}
 
@@ -61,11 +114,25 @@ public class PersistenceConfig {
 	}
 
 	private Properties additionalProperties() {
+		final String dbms = env.getProperty("spring.datasource.dbms", "");
+
 		Properties properties = new Properties();
 		properties.put("hibernate.hbm2ddl.auto", env.getProperty("spring.jpa.hibernate.ddl-auto", ""));
-		properties.put("hibernate.dialect", env.getProperty("spring.jpa.database-platform", ""));
 		properties.put("hibernate.show_sql", env.getProperty("spring.jpa.show-sql", "false"));
 		properties.put("hibernate.globally_quoted_identifiers", env.getProperty("spring.jpa.hibernate.globally_quoted_identifiers", "false"));
+
+		switch (dbms) {
+			case "postgresql":
+				properties.put("hibernate.dialect", "org.hibernate.dialect.PostgreSQL82Dialect");
+				break;
+			case "mysql":
+				properties.put("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+				break;
+			default:
+			case "memory":
+				properties.put("hibernate.dialect", "org.hibernate.dialect.H2Dialect");
+		}
+
 		return properties;
 	}
 
