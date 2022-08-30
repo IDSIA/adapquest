2fd0db3599ceccb2c37059b9b8b844581fac4201
Claudio Bonesana <claudio.bonesana@supsi.ch>
2022-08-30 16:10:43 +0200

    Added support for Keycloak.

---
+++ AdapQuestBackend/src/main/java/ch/idsia/adaptive/backend/config/KeycloakConfig.java
@@ -0,0 +1,17 @@
+package ch.idsia.adaptive.backend.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
+public class KeycloakConfig {
+
+	@Bean
+	public KeycloakSpringBootConfigResolver keycloakConfigResolver() {
+		return new KeycloakSpringBootConfigResolver();
+	}
+
+}
---
+++ AdapQuestBackend/src/main/java/ch/idsia/adaptive/backend/config/SecurityKeycloakConfig.java
@@ -0,0 +1,62 @@
+package ch.idsia.adaptive.backend.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+@KeycloakConfiguration
+@AutoConfigureAfter(SecurityConfig.class)
+@ConditionalOnProperty(name = "keycloak.enabled", havingValue = "true")
+class SecurityKeycloakConfig extends KeycloakWebSecurityConfigurerAdapter {
+	private static final Logger logger = LoggerFactory.getLogger(SecurityKeycloakConfig.class);
+
+	@Value("${adapquest.keycloak.role}")
+	private String role = "user";
+
+	@Value("${adapquest.keycloak.admin}")
+	private String admin = "admin";
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) {
+		final SimpleAuthorityMapper simpleAuthorityMapper = new SimpleAuthorityMapper();
+		simpleAuthorityMapper.setPrefix("ROLE_");
+		simpleAuthorityMapper.setConvertToUpperCase(true);
+
+		final KeycloakAuthenticationProvider keycloakAuthenticationProvider = keycloakAuthenticationProvider();
+		keycloakAuthenticationProvider.setGrantedAuthoritiesMapper(simpleAuthorityMapper);
+		auth.authenticationProvider(keycloakAuthenticationProvider);
+	}
+
+	@Bean
+	@Override
+	protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+		return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.authorizeRequests()
+				.antMatchers("/", "/css/**", "/webjars/**", "/img/**").permitAll()
+				.and()
+				.authorizeRequests()
+				.antMatchers("/demo**", "/survey**").hasRole(role)
+				.antMatchers("/console*").hasRole(admin)
+				.anyRequest()
+				.authenticated();
+		;
+	}
+}
